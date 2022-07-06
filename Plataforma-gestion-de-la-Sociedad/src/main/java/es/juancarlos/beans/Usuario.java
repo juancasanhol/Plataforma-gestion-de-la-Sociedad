aa4f66e979848ff7549b803fca1d29e2b5f2818a
ajog98 <105661442+ajog98@users.noreply.github.com>
2022-07-06 15:58:13 +0200

    Edicion de usuario terminada y funcional

--- Plataforma-gestion-de-la-Sociedad/src/main/java/es/juancarlos/beans/Usuario.java
+++ Plataforma-gestion-de-la-Sociedad/src/main/java/es/juancarlos/beans/Usuario.java
@@ -286,7 +286,7 @@ public class Usuario implements Serializable {
     }
 
     //Constructor usado para la insercion de datos en la edicion del usuario
-    public Usuario(int NumIntId, String Nombre, String Apellidos, String FechaAlta, String FechaBaja, String TipoDoc, String NumDoc, String Telefono, String Correo, String PersonaReferencia, String Sexo, String FechaNac, String PaisOrigen, String Nacionalidad, Boolean PerteneceMinoria, String Minoria, Boolean SolicitaAyudaFarmaceutica, String TratSanitario, Boolean Drogodependencia, String TipoDiscapacidad, String GradoDiscapacidad, Boolean PermisoResidencia, Boolean PermisoTrabajo, Boolean CarnetConducir, String TipoCarnetConducir, String SituacionLaboral, String UltTrabajo, String PrefLaboral, String NivelEstudios, String FormacionComp, Boolean EstaEstudiando, Boolean FracasoEscolar, String CentroEst, float Importe, String OrigenIngresos, String Denominacion, String Direccion, Boolean FamiliaMonoparental, Boolean SinHogar, float CosteVivienda, String MotivoCoste, Boolean EstaBanco, String FechaAlta_BancoAlimentos, String FechaBaja_BancoAlimentos, String FechaOrientacion, String Beneficiario) {
+    /*public Usuario(int NumIntId, String Nombre, String Apellidos, String FechaAlta, String FechaBaja, String TipoDoc, String NumDoc, String Telefono, String Correo, String PersonaReferencia, String Sexo, String FechaNac, String PaisOrigen, String Nacionalidad, Boolean PerteneceMinoria, String Minoria, Boolean SolicitaAyudaFarmaceutica, String TratSanitario, Boolean Drogodependencia, String TipoDiscapacidad, String GradoDiscapacidad, Boolean PermisoResidencia, Boolean PermisoTrabajo, Boolean CarnetConducir, String TipoCarnetConducir, String SituacionLaboral, String UltTrabajo, String PrefLaboral, String NivelEstudios, String FormacionComp, Boolean EstaEstudiando, Boolean FracasoEscolar, String CentroEst, float Importe, String OrigenIngresos, String Denominacion, String Direccion, Boolean FamiliaMonoparental, Boolean SinHogar, float CosteVivienda, String MotivoCoste, Boolean EstaBanco, String FechaAlta_BancoAlimentos, String FechaBaja_BancoAlimentos, String FechaOrientacion, String Beneficiario) {
         this.NumIntId = NumIntId;
         this.Nombre = Nombre;
         this.Apellidos = Apellidos;
@@ -333,6 +333,69 @@ public class Usuario implements Serializable {
         this.FechaBaja_BancoAlimentos = FechaBaja_BancoAlimentos;
         this.FechaOrientacion = FechaOrientacion;
         this.Beneficiario = Beneficiario;
+    }*/
+
+    public Usuario(int NumIntId, String Nombre, String Apellidos, String FechaAlta, String FechaBaja, String TipoDoc, String NumDoc, String Telefono, String Correo, String PersonaReferencia, String Sexo, String FechaNac, String PaisOrigen, String Nacionalidad, Boolean PerteneceMinoria, String Minoria, List<Observaciones> observaciones_id, List<FicheroAdjunto> ficheros_usuario, Boolean SolicitaAyudaFarmaceutica, String TratSanitario, Boolean Drogodependencia, String TipoDiscapacidad, String GradoDiscapacidad, List<Observaciones> observaciones_sanitarias, Boolean PermisoResidencia, Boolean PermisoTrabajo, Boolean CarnetConducir, String TipoCarnetConducir, List<ValorDesplegable> otros_carnets, List<ProfesionObservaciones> profesion_observaciones, String SituacionLaboral, String UltTrabajo, String PrefLaboral, List<ValorDesplegable> bolsa_trabajo, List<Observaciones> observaciones_datos_laborales, String NivelEstudios, String FormacionComp, Boolean EstaEstudiando, Boolean FracasoEscolar, String CentroEst, List<Observaciones> observaciones_formacion, float Importe, String OrigenIngresos, List<Observaciones> observaciones_ingresos, String Denominacion, String Direccion,String Localidad, Boolean FamiliaMonoparental, Boolean SinHogar, float CosteVivienda, String MotivoCoste, List<Observaciones> observaciones_ficha_convivencia, List<FicheroAdjunto> ficheros_unidadconvivencia, Boolean EstaBanco, String FechaAlta_BancoAlimentos, String FechaBaja_BancoAlimentos, List<BancoAlimentos> lista_recogidas, String FechaOrientacion, String Beneficiario, List<Observaciones> observaciones_orientacion) {
+        this.NumIntId = NumIntId;
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.FechaAlta = FechaAlta;
+        this.FechaBaja = FechaBaja;
+        this.TipoDoc = TipoDoc;
+        this.NumDoc = NumDoc;
+        this.Telefono = Telefono;
+        this.Correo = Correo;
+        this.PersonaReferencia = PersonaReferencia;
+        this.Sexo = Sexo;
+        this.FechaNac = FechaNac;
+        this.PaisOrigen = PaisOrigen;
+        this.Nacionalidad = Nacionalidad;
+        this.PerteneceMinoria = PerteneceMinoria;
+        this.Minoria = Minoria;
+        this.observaciones_id = observaciones_id;
+        this.ficheros_usuario = ficheros_usuario;
+        this.SolicitaAyudaFarmaceutica = SolicitaAyudaFarmaceutica;
+        this.TratSanitario = TratSanitario;
+        this.Drogodependencia = Drogodependencia;
+        this.TipoDiscapacidad = TipoDiscapacidad;
+        this.GradoDiscapacidad = GradoDiscapacidad;
+        this.observaciones_sanitarias = observaciones_sanitarias;
+        this.PermisoResidencia = PermisoResidencia;
+        this.PermisoTrabajo = PermisoTrabajo;
+        this.CarnetConducir = CarnetConducir;
+        this.TipoCarnetConducir = TipoCarnetConducir;
+        this.otros_carnets = otros_carnets;
+        this.profesion_observaciones = profesion_observaciones;
+        this.SituacionLaboral = SituacionLaboral;
+        this.UltTrabajo = UltTrabajo;
+        this.PrefLaboral = PrefLaboral;
+        this.bolsa_trabajo = bolsa_trabajo;
+        this.observaciones_datos_laborales = observaciones_datos_laborales;
+        this.NivelEstudios = NivelEstudios;
+        this.FormacionComp = FormacionComp;
+        this.EstaEstudiando = EstaEstudiando;
+        this.FracasoEscolar = FracasoEscolar;
+        this.CentroEst = CentroEst;
+        this.observaciones_formacion = observaciones_formacion;
+        this.Importe = Importe;
+        this.OrigenIngresos = OrigenIngresos;
+        this.observaciones_ingresos = observaciones_ingresos;
+        this.Denominacion = Denominacion;
+        this.Direccion = Direccion;
+        this.Localidad = Localidad;
+        this.FamiliaMonoparental = FamiliaMonoparental;
+        this.SinHogar = SinHogar;
+        this.CosteVivienda = CosteVivienda;
+        this.MotivoCoste = MotivoCoste;
+        this.observaciones_ficha_convivencia = observaciones_ficha_convivencia;
+        this.ficheros_unidadconvivencia = ficheros_unidadconvivencia;
+        this.EstaBanco = EstaBanco;
+        this.FechaAlta_BancoAlimentos = FechaAlta_BancoAlimentos;
+        this.FechaBaja_BancoAlimentos = FechaBaja_BancoAlimentos;
+        this.lista_recogidas = lista_recogidas;
+        this.FechaOrientacion = FechaOrientacion;
+        this.Beneficiario = Beneficiario;
+        this.observaciones_orientacion = observaciones_orientacion;
     }
 
     
@@ -817,5 +880,6 @@ public class Usuario implements Serializable {
         this.observaciones_orientacion = observaciones_orientacion;
     }
 
+    
    
 }
---
+++ Plataforma-gestion-de-la-Sociedad/src/main/java/es/juancarlos/controllers/GuardarCambiosUsuario.java
@@ -0,0 +1,128 @@
+package es.juancarlos.controllers;
+
+import es.juancarlos.beans.FicheroAdjunto;
+import es.juancarlos.beans.Observaciones;
+import es.juancarlos.beans.Usuario;
+import es.juancarlos.daofactory.DAOFactory;
+import es.juancarlos.interfaces.IGenericoDAO;
+import es.juancarlos.models.GuardarFicheros;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Juan Carlos Sánchez Holguín
+ */
+@MultipartConfig
+@WebServlet(name = "GuardarCambiosUsuario", urlPatterns = {"/GuardarCambiosUsuario"})
+public class GuardarCambiosUsuario extends HttpServlet {
+
+    //para devolver un solo objeto
+    JSONObject objeto = null;
+
+    //para devolver uno o varios objetos (varios preferiblemente)
+    JSONArray arrayJSON = null;
+
+    /**
+     * Servlet usado para consultas Ajax de las diferentes páginas
+     *
+     * @param request Servlet request
+     * @param response Servlet response
+     * @throws ServletException Si ocurre un error específico del Servlet
+     * @throws IOException Si ocurre un error de I/O
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        DAOFactory daof = DAOFactory.getDAOFactory();
+        IGenericoDAO gdao = daof.getGenericoDAO();
+        int id = Integer.parseInt(request.getSession().getAttribute("id").toString());
+        Usuario u = (Usuario) gdao.getById(id, Usuario.class);
+        Boolean minoria= false, ayudafarmaceutica= false, drogodependencia= false, permisoresidencia= false, permisotrabajo= false, carnetconducir= false, estaestudiando= false, fracasoescolar= false, familiamonoparental= false, sinhogar = false, estabanco = false;
+        if (request.getParameter("PerteneceMinoria") != null) {
+            minoria = true;
+        }
+        if (request.getParameter("SolicitaAyudaFarmaceutica") != null) {
+            ayudafarmaceutica = true;
+        }
+        if (request.getParameter("Drogodependencia") != null) {
+            drogodependencia = true;
+        }
+        if (request.getParameter("PermisoResidencia") != null) {
+            permisoresidencia = true;
+        }
+        if (request.getParameter("PermisoTrabajo") != null) {
+            permisotrabajo = true;
+        }
+        if (request.getParameter("CarnetConducir") != null) {
+            carnetconducir = true;
+        }
+        if (request.getParameter("EstaEstudiando") != null) {
+            estaestudiando = true;
+        }
+        if (request.getParameter("FracasoEscolar") != null) {
+            fracasoescolar = true;
+        }
+        if (request.getParameter("FamiliaMonoparental") != null) {
+            familiamonoparental = true;
+        }
+        if (request.getParameter("SinHogar") != null) {
+            sinhogar = true;
+        }
+        if (request.getParameter("EstaBanco") != null) {
+            estabanco = true;
+        }
+        gdao.insertOrUpdate(new Usuario(id,request.getParameter("Nombre"), request.getParameter("Apellidos"), request.getParameter("FechaAlta"), request.getParameter("FechaBaja"), request.getParameter("TipoDoc"), request.getParameter("NumDoc"), request.getParameter("Telefono"), request.getParameter("Correo"), request.getParameter("PersonaReferencia"), request.getParameter("Sexo"), request.getParameter("FechaNac"), request.getParameter("PaisOrigen"), request.getParameter("Nacionalidad"), minoria, request.getParameter("Minoria"), u.getObservaciones_id(), u.getFicheros_usuario(), ayudafarmaceutica,request.getParameter("TratSanitario"),drogodependencia,request.getParameter("TipoDiscapacidad"),request.getParameter("GradoDiscapacidad"),u.getObservaciones_sanitarias(),permisoresidencia,permisotrabajo,carnetconducir,request.getParameter("TipoCarnetConducir"),u.getOtros_carnets(),u.getProfesion_observaciones(),request.getParameter("SituacionLaboral"),request.getParameter("UltTrabajo"),request.getParameter("PrefLaboral"),u.getBolsa_trabajo(),u.getObservaciones_datos_laborales(), request.getParameter("NivelEstudios"),request.getParameter("FormacionComp"),estaestudiando,fracasoescolar,request.getParameter("CentroEst"),u.getObservaciones_formacion(),Float.parseFloat(request.getParameter("Importe")),request.getParameter("OrigenIngresos"),u.getObservaciones_ingresos(),request.getParameter("Denominacion"),request.getParameter("Direccion"), request.getParameter("Localidad"),familiamonoparental,sinhogar,Float.parseFloat(request.getParameter("CosteVivienda")),request.getParameter("MotivoCoste"),u.getObservaciones_ficha_convivencia(),u.getFicheros_unidadconvivencia(),estabanco,request.getParameter("FechaAlta_BancoAlimentos"),request.getParameter("FechaBaja_BancoAlimentos"),u.getLista_recogidas(),request.getParameter("FechaOrientacion"),request.getParameter("Beneficiario"),u.getObservaciones_orientacion()));
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
