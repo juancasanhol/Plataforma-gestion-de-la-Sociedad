1754c51169da5257bfd78be96cda307281ea6c26
juancasanhol <juancasanhol@gmail.com>
2022-07-09 16:55:28 +0200

    correcion para el loguin junto a la funcionalodad de entrar con el "intro"

--- Plataforma-gestion-de-la-Sociedad/src/main/webapp/js/login.js
+++ Plataforma-gestion-de-la-Sociedad/src/main/webapp/js/login.js
@@ -1,23 +1,45 @@
 $(document).ready(function () {
-   
+
+    $("body").keyup(function (event){
+
+       if(event.which==13){
+
+        Loging();
+
+       }
+
+    });
+
     $("#login").click(function (){
 
-        $.ajax({
-            type: "post",
-            url: "Ajax",
-            data: {
-                accion: "login",
-                usuario: $("#usuario").val(),
-                passwd: $("#passwd").val()
-            },
-            success: function (respuesta) {
-                location.href ='html/MenuPrincipal/Menu.html';
-            },
-            error: function () {
-                console.log("ERROR!! error al comprobar el aceso");
-            }
-        });
+        Loging();
 
     });
     
 });
+
+function Loging(){
+
+    $.ajax({
+        type: "post",
+        url: "Ajax",
+        data: {
+            accion: "login",
+            usuario: $("#usuario").val(),
+            passwd: $("#passwd").val()
+        },
+        success: function (respuesta) {
+
+            if(respuesta.aceso){
+                location.href ='html/MenuPrincipal/Menu.html';
+            }else{
+                
+            }
+          
+        },
+        error: function () {
+            console.log("ERROR!! error al comprobar el aceso");
+        }
+    });
+
+}
