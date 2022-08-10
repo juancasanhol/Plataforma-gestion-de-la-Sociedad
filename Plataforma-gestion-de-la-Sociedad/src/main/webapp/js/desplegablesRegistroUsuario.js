618e7f4e4da3a2bf6161797f7a43428c84acc9b1
juancasanhol <juancasanhol@gmail.com>
2022-08-11 00:53:48 +0200

    funcionalidad de carnet mejorada

--- Plataforma-gestion-de-la-Sociedad/src/main/webapp/js/desplegablesRegistroUsuario.js
+++ Plataforma-gestion-de-la-Sociedad/src/main/webapp/js/desplegablesRegistroUsuario.js
@@ -15,11 +15,29 @@ $(document).ready(function () {
                 if(option.nacionalidad!==undefined){$("#DesplegablesNacionalidad").append('<option value="' + option.nacionalidad + '">');}
                 if(option.minoriaetnica!==undefined){$("#DesplegablesMinoria").append('<option value="' + option.minoriaetnica + '">');}
                 if(option.usuarios!==undefined){$("#DesplegablesPersonaReferencia").append('<option value="' + option.usuarios + '">');}
+                if(option.tipoCarnetConducir!==undefined){$("#tiposCarnet").append('<tr><td>'+option.tipoCarnetConducir+'</td><td><button type="button" id='+option.tipoCarnetConducir+' class="btn btn-success addCarnet">añadir</button></td></tr>');}    
             });
-
+            
         },
         error: function () {
             console.log("ERROR CARGANDO DESPLEGABLES");
         }
     });
+    
+    $(document).on('click', '.addCarnet', function () {
+        var nombreD = $(this).attr("id");
+        $("#DesplegablesTipoCarnetConducir").append('<option value='+nombreD+'>'+nombreD+'</option>'); 
+        $("#tiposCarnetDel").append('<tr><td>'+nombreD+'</td><td><button type="button" id='+nombreD+' class="btn btn-danger delCarnet">eliminar</button></td></tr>'); 
+        $(this).closest('tr').remove();
+    });
+
+    $(document).on('click', '.delCarnet', function () {
+        var nombreD = $(this).attr("id");
+        console.log(nombreD);
+        $("option[value='"+nombreD+"']").remove(); 
+        $("#tiposCarnet").append('<tr><td>'+nombreD+'</td><td><button type="button" id='+nombreD+' class="btn btn-success addCarnet">añadir</button></td></tr>'); 
+        $(this).closest('tr').remove();
+    });
+
+
 });
