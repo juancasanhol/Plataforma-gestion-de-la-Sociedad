d03caa40499a5af07449a76b1a92ad9e4715aef8
Pablorodrivill <103128923+Nesuu21@users.noreply.github.com>
2023-02-25 21:53:38 +0100

    Principio de js Crear COnferencias Santa Maria

---
+++ Plataforma-gestion-de-la-Sociedad/src/main/webapp/js/ConferenciSantaMaria.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+ $(document).on('click', '.add', function () {
+        
+
+        $.ajax({
+            type: "post",
+            url: "../../Ajax",
+            data: {
+                accion: "addConferenciaSantaMaria",
+//                Hay que poner cada campo y recogerlo
+            },
+            success: function (respuesta) {
+
+            },
+            error: function () {
+                console.log("ERROR ");
+            },
+
+        });
+    }); 
+});
+
