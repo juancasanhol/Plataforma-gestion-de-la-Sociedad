9c7a808987661d11d861d148bb019e6589b0654f
Pablorodrivill <103128923+Nesuu21@users.noreply.github.com>
2023-03-03 02:00:01 +0100

    Update nuevoUsuario.js

--- Plataforma-gestion-de-la-Sociedad/src/main/webapp/js/nuevoUsuario.js
+++ Plataforma-gestion-de-la-Sociedad/src/main/webapp/js/nuevoUsuario.js
@@ -12,10 +12,21 @@ $(document).ready(function () {
                 tipoDoc: $("TipoDoc").val(),
                 personaReferencia: $("personaReferencia").val(),
                 nacionalidad: $("nacionalidad").val(),
-//                nombre: $("nombre").val(),
-//                nombre: $("nombre").val(),
-//                nombre: $("nombre").val(),
-//                HAY QUE PILLAR LOS DATOS
+                nivelEstudios: $("NivelEstudios").val(),
+                centroEst: $("CentroEst").val(),
+                formacionComp: $("FormacionComp").val(),
+                denominacion: $("Denominacion").val(),
+                direccion: $("Direccion").val(),
+                costevivienda:$("CosteVivienda").val(),
+                localidad: $("Localidad").val(),
+                motivoCoste:$("MotivoCoste").val(),
+                observacionesConvivencia:$("ObservacionesConvivencia").val(),
+                fechaAlta_BancoAlimentos : $("FechaAlta_BancoAlimentos").val(),
+                fechaBaja_BancoAlimentos: $("FechaBaja_BancoAlimentos").val(),
+                fechaOrientacion : $("FechaOrientacion").val(),
+                beneficiario : $("Beneficiario").val(),
+                
+//              $("#TipoDoc").val()
             },
             success: function (respuesta) {
 
